e9cca64ab288f3ef6f3f77831520576d2d273a28
Andrei Grishachkov <Andreigrishachkov1536@gmail.com>
2026-08-11 22:01:17 +0300

    Сделал ДЗ 1 и создал файл под 2 задачу

--- homework11/ex1.py
+++ homework11/ex1.py
@@ -8,6 +8,6 @@ def power_factory(power):
 square = power_factory(2)
 cube = power_factory(3)
 
-print(square(5))  # 25
-print(cube(2))  # 8
-print(square(10))  # 100
+print(square(5))
+print(cube(2))
+print(square(10))
