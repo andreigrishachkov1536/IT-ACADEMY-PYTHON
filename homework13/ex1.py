601b29d5b98bd4e01ebd84ca78c8780f1d466bdc
Andrei Grishachkov <Andreigrishachkov1536@gmail.com>
2026-08-15 22:35:37 +0300

    Сделал ветку и 1ую задачу 13ой домашки

---
+++ homework13/ex1.py
@@ -0,0 +1,32 @@
+while True:
+
+        chislo= input("Введите число от 0 до 100: ")
+        if chislo=="exit":
+            break
+        try:
+            chislo = int(chislo)
+
+            if chislo > 100:
+                print("Слишком большое число")
+            elif chislo < 0:
+                print("Слишком маленькое число")
+
+            else:
+                print(f"Число принято {chislo}")
+                break
+
+        except ValueError as e:
+            print("Ошибка: введите число")
+
+
+
+
+
+
+
+
+
+
+
+
+
